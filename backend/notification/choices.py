af4f0defd5ca1250ef928aa74a7ab55f4a15601c
Rabi Subedi Chettri <rsubedi336@gmail.com>
2021-11-17 18:28:24 +0545

    removed notification function and chaneged choices

---
+++ backend/notification/choices.py
@@ -0,0 +1,11 @@
+NotificationType = [
+    ('pr', 'private'),
+    ('pu', 'public'),
+]
+NotificationStatus = [
+    ('w', "warning"),
+    ('d', "danger"),
+    ('n', "normal"),
+    ('s', "success"),
+    ('s', 'fail'),
+]
--- backend/notification/models.py
+++ backend/notification/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from account.models import User
+from .choices import *
 
-# Create your models here.
+
+class Notification(models.Model):
+    type = models.CharField(max_length=2, choices=NotificationType)
+    status = models.CharField(max_length=2, choices=NotificationStatus)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_created=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.id)
+
+    def create_n(self):
+        pass
+
+    def remove_n(self):
+        pass
+
+    def update_n(self):
+        pass
+
+    def read_n(self):
+        pass
